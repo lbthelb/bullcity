5666221398795a5e766c0e129437dc634764f9fb
abc <abc@example.com>
2021-01-13 15:16:29 -0500

    we got bones

---
+++ src/components/navbar/navbar.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './navbar.styles.css'; 
+
+export const Navbar = (props) => (
+    <div className='navbar' >
+        <a href=''>Home</a>
+        <a href=''>Gallery</a>
+        <a href=''>About</a>
+        <a href=''>Contact</a>
+    </div>
+)
